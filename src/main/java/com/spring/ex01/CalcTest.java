2f6d3b62af563903968a181a509dbf928c2d3791
mungkeu <101312446+mungkeu@users.noreply.github.com>
2022-10-03 00:16:29 +0900

    Spring : AOP

---
+++ src/main/java/com/spring/ex01/CalcTest.java
@@ -0,0 +1,22 @@
+package com.spring.ex01;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CalcTest {
+    public static void main(String[] args) {
+        // AOPTest.xml을 읽어 들여 빈을 생성한다.
+        ApplicationContext context = new ClassPathXmlApplicationContext("AOPTest.xml");
+        // id가 proxyCal인 빈에 접근한다.
+        Calculator cal = (Calculator) context.getBean("proxyCal");
+
+        // 메서드 호출 전후에 어드바이스 빈이 적용된다.
+        cal.add(100, 20);
+        System.out.println();
+        cal.subtract(100, 20);
+        System.out.println();
+        cal.multiply(100, 20);
+        System.out.println();
+        cal.divide(100, 20);
+    }
+}
